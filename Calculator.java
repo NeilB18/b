9244b2ebd5965cc3d6044311834a7b15c62e1bcd
Neil <82923875+NeilB18@users.noreply.github.com>
2024-10-15 13:01:58 -0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.utils.*;
+public class Calculator{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.print("enter first num: ");
+    int x = scan.nextInt();
+    System.out.println();
+  }
+
+}
